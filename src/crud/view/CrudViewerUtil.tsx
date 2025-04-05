86f3178294a761a0a530e66f0c98615962528700
PandukaNandara <pandukanandara@outlook.com>
2025-04-05 17:02:39 +0530

    Refactor getRendererValueCrudViewer function to improve error handling and streamline rendering logic for various field types in CrudViewerUtil component.

--- src/crud/view/CrudViewerUtil.tsx
+++ src/crud/view/CrudViewerUtil.tsx
@@ -22,128 +22,140 @@ export function getRendererValueCrudViewer<T>({
   render,
   ...props
 }: CrudFieldProps<T>) {
-  return type === "object"
-    ? (e, value, i) =>
-        typeof render === "function" ? render(e, value, i) : String()
-    : type === "select"
-    ? (e, value, i) => {
-        const selectProps = props as any as SelectCrudField<{}>;
-        let v = selectProps.multiple
-          ? Array.isArray(e)
-            ? e.map((e) => e?.[selectProps.innerFieldLabel ?? "name"])
-            : undefined
-          : e?.[selectProps.innerFieldLabel ?? "name"];
-        if ((!v && typeof e === "string") || typeof e === "number") v = e;
-        return typeof render === "function"
-          ? render(v, value, i)
-          : Array.isArray(v)
-          ? v.join(", ")
-          : v;
-      }
-    : type === "number"
-    ? (e, value, i) =>
-        typeof render === "function"
-          ? render(e, value, i)
-          : (props as NumberBasedFieldProps<T>)?.int
-          ? NumberUtil.toInt(e, (props as NumberBasedFieldProps<T>).formatted)
-          : NumberUtil.toMoney(e)
-    : type === "enum"
-    ? (e, value, i) => {
-        const v = t(
-          (props as any as EnumCrudField<{}>)?.translation?.[e ?? ""] ?? e
-        ) as string;
-        const tagRender = (props as any as EnumCrudField<{}>)?.tagRender;
-        if (typeof render === "function") {
-          return render(e, value, i);
+  try {
+    return type === "object"
+      ? (e, value, i) =>
+          typeof render === "function" ? render(e, value, i) : String()
+      : type === "select"
+      ? (e, value, i) => {
+          const selectProps = props as any as SelectCrudField<{}>;
+          const v = selectProps.multiple
+            ? Array.isArray(e)
+              ? e.map((e) => e?.[selectProps.innerFieldLabel ?? "name"])
+              : undefined
+            : e?.[selectProps.innerFieldLabel ?? "name"];
+          return typeof render === "function"
+            ? render(v, value, i)
+            : Array.isArray(v)
+            ? v.join(", ")
+            : v;
         }
-        if (typeof tagRender === "object") {
-          const tagProps = tagRender[e];
-          if (tagProps) {
-            return <Tag color={tagProps.color}>{v}</Tag>;
+      : type === "number"
+      ? (e, value, i) =>
+          typeof render === "function"
+            ? render(e, value, i)
+            : (props as NumberBasedFieldProps<T>)?.int
+            ? NumberUtil.toInt(e, (props as NumberBasedFieldProps<T>).formatted)
+            : NumberUtil.toMoney(e)
+      : type === "enum"
+      ? (e, value, i) => {
+          const propsEnum = props as any as EnumCrudField<{}>;
+          if (typeof render === "function") {
+            return render(e, value, i);
           }
+          if (propsEnum.multiple) {
+            const ar = Array.isArray(e) ? e : e ? [e] : [];
+            if (propsEnum?.translation) {
+              return ar
+                ?.map((e) => t(propsEnum?.translation?.[e ?? ""] ?? e))
+                .join(", ");
+            } else {
+              return ar?.join(", ");
+            }
+          }
+          const val = propsEnum?.translation?.[e ?? ""] ?? e;
+          const v = t(val);
+
+          return v;
         }
-        return v;
-      }
-    : type === "date"
-    ? (e, value, i) => {
-        if (!e) return "-";
-        const v = (props as any as DateBasedFieldProps<{}>)?.formatTime
-          ? DateUtil.formatDateTime(e)
-          : DateUtil.formatDate(e);
-        return typeof render === "function" ? render(e, value, i) : v;
-      }
-    : type === "checkbox"
-    ? (e, value, i) =>
-        typeof render === "function" ? (
-          render(e, value, i)
-        ) : e ? (
-          <CheckOutlined />
-        ) : (
-          <CloseOutlined />
-        )
-    : type === "image"
-    ? (e, value, i) => {
-        return typeof render === "function" ? (
-          render(e, value, i)
-        ) : (
-          <ImageCrudCellValue
-            value={e}
-            provider={(props as ImageCrudField<T>).provider}
-          />
-        );
-      }
-    : type === "file"
-    ? (e, value, i) => {
-        return typeof render === "function" ? (
-          render(e, value, i)
-        ) : (
-          <FileCrudCellValue
-            value={e}
-            provider={(props as ImageCrudField<T>).provider}
-          />
-        );
-      }
-    : type === "time"
-    ? (e, value, i) => {
-        if (!e) return "-";
-        const format = (props as any as TimeBasedFieldProps<{}>)?.format;
-        const use12Hours = (props as any as TimeBasedFieldProps<{}>)
-          ?.use12Hours;
-        const valueFormatted = DateUtil.formatTime(
-          e,
-          format ? format : use12Hours ? "hh:mm:ss A" : undefined
-        );
-        return typeof render === "function"
-          ? render(e, value, i)
-          : valueFormatted;
-      }
-    : type === "color"
-    ? (e, value, i) => {
-        return typeof render === "function" ? (
-          render(e, value, i)
-        ) : typeof e === "string" && e.startsWith("#") ? (
-          <Tooltip title={e}>
-            <Avatar style={{ backgroundColor: e }}></Avatar>
-          </Tooltip>
-        ) : (
-          String(e)
-        );
-      }
-    : type === "textarea"
-    ? (e, value, i) => {
-        const truncated =
-          (props as any as TextAreaBasedFieldProps<{}>)?.truncated ?? 1;
-        return typeof render === "function" ? (
-          render(e, value, i)
-        ) : !truncated ? (
-          e
-        ) : (
-          <ShowMore lines={truncated === true ? 1 : (truncated as number)}>
-            {e}
-          </ShowMore>
-        );
-      }
-    : typeof render === "function"
-    ? render
-    : (e) => e;
+      : type === "date"
+      ? (e, value, i) => {
+          if (!e) return "-";
+          const v = (props as any as DateBasedFieldProps<{}>)?.formatTime
+            ? DateUtil.formatDateTime(e)
+            : DateUtil.formatDate(e);
+          return typeof render === "function" ? render(e, value, i) : v;
+        }
+      : type === "checkbox"
+      ? (e, value, i) =>
+          typeof render === "function" ? (
+            render(e, value, i)
+          ) : e ? (
+            <CheckOutlined />
+          ) : (
+            <CloseOutlined />
+          )
+      : type === "image"
+      ? (e, value, i) => {
+          return typeof render === "function" ? (
+            render(e, value, i)
+          ) : (
+            <ImageCrudCellValue
+              value={e}
+              provider={(props as ImageCrudField<T>).provider}
+            />
+          );
+        }
+      : type === "file"
+      ? (e, value, i) => {
+          return typeof render === "function" ? (
+            render(e, value, i)
+          ) : (
+            <FileCrudCellValue
+              value={e}
+              provider={(props as ImageCrudField<T>).provider}
+            />
+          );
+        }
+      : type === "time"
+      ? (e, value, i) => {
+          if (!e) return "-";
+          const format = (props as any as TimeBasedFieldProps<{}>)?.format;
+          const use12Hours = (props as any as TimeBasedFieldProps<{}>)
+            ?.use12Hours;
+          const valueFormatted = DateUtil.formatTime(
+            e,
+            format ? format : use12Hours ? "hh:mm:ss A" : undefined
+          );
+          return typeof render === "function"
+            ? render(e, value, i)
+            : valueFormatted;
+        }
+      : type === "color"
+      ? (e, value, i) => {
+          return typeof render === "function" ? (
+            render(e, value, i)
+          ) : typeof e === "string" && e.startsWith("#") ? (
+            <Tooltip title={e}>
+              <Avatar style={{ backgroundColor: e }}></Avatar>
+            </Tooltip>
+          ) : (
+            String(e)
+          );
+        }
+      : type === "textarea"
+      ? (e, value, i) => {
+          const truncated =
+            (props as any as TextAreaBasedFieldProps<{}>)?.truncated ?? 1;
+          return typeof render === "function" ? (
+            render(e, value, i)
+          ) : !truncated ? (
+            e
+          ) : (
+            <ShowMore lines={truncated === true ? 1 : (truncated as number)}>
+              {e}
+            </ShowMore>
+          );
+        }
+      : typeof render === "function"
+      ? render
+      : (e, value, i) => e;
+  } catch (e) {
+    console.warn(
+      "An error occurred while rendering the value for field: " +
+        String(props.name),
+      e
+    );
+    return "-" as any;
+  }
 }
